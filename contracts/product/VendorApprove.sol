d5cb4d2f86498f6b8b49dbff61752c94ebe49025
bcshop-io <kondrahanov@mail.ru>
2018-04-29 11:10:25 +0700

    VendorApprove contract

---
+++ contracts/product/VendorApprove.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.4.18;
+
+import "../token/IERC20Token.sol";
+import "../token/TokenHolder.sol";
+
+/**@dev Vendors use this contract to request approval. 
+Managers should call grantApprove function to resolve the case.
+Also serves for storage of vendor token payment while approve is being processed */
+contract VendorApprove is TokenHolder {
+	
+    //
+    // Events
+    event ApprovalRequested(address indexed sender);
+    event ApprovalGranted(address indexed sender, bool state);
+
+
+    //
+    // Storage data
+    IERC20Token public token;
+    uint256 public tokensForApproval;
+    mapping(address => bool) public allowedUsers;
+    mapping(address => uint256) public requests;
+
+
+    //
+    // Modifiers
+    modifier allowedUserOnly() {
+        require(allowedUsers[msg.sender]);
+        _;
+    }
+
+
+    //
+    // Methods
+
+    function VendorApprove(IERC20Token _token, uint256 _tokensForApproval) public {
+        setParams(_token, _tokensForApproval);
+    }
+
+    function setParams(IERC20Token _token, uint256 _tokensForApproval) public ownerOnly {
+        token = _token;
+        tokensForApproval = _tokensForApproval;
+    }
+
+    /**@dev Call this to request approval, throws exception if there is another request pending */
+    function requestApprove() public {
+        require(requests[msg.sender] == 0);
+
+        token.transferFrom(msg.sender, this, tokensForApproval);
+        requests[msg.sender] = tokensForApproval;
+
+        ApprovalRequested(msg.sender);
+    }
+
+    /**@dev Approval manager calls this method to grant or deny approval */
+    function grantApprovals(address[] senders, bool[] states) 
+        public
+        allowedUserOnly
+    {
+        require(senders.length == states.length);
+
+        for(uint256 i = 0; i < senders.length; ++i) {
+            grantApprove(senders[i], states[i]);
+        }
+    }
+
+    function grantApprove(address sender, bool state) 
+        internal 
+    {
+        require(requests[sender] > 0);
+
+        ApprovalGranted(sender, state);
+
+        if(!state) {
+            require(token.transfer(sender, requests[msg.sender]));
+            requests[msg.sender] = 0;
+        }
+    }
+}
