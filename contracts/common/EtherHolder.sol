1f38fa69efed455e87cc3a119d080d4b18b519bb
bcshop-io <kondrahanov@mail.ru>
2018-04-29 11:09:37 +0700

    Add Manageable ancestor

--- contracts/common/EtherHolder.sol
+++ contracts/common/EtherHolder.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.18;
 
-import "../common/Owned.sol";
+import "../common/Manageable.sol";
 
 /**@dev Contract that can hold and receive Ether and transfer it to anybody */
-contract EtherHolder is Owned {
+contract EtherHolder is Manageable {
     
     //
     // Methods
@@ -12,7 +12,7 @@ contract EtherHolder is Owned {
     } 
 
     /**@dev withdraws amount of ether to specific adddress */
-    function withdrawEtherTo(uint256 amount, address to) public ownerOnly {
+    function withdrawEtherTo(uint256 amount, address to) public managerOnly {
         to.transfer(amount);
     }
 
